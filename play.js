7fdba508ed82f1b2409a7ec75c486219452f457d
Rishard C <rishard.c@hotmail.com>
2021-12-01 16:49:21 -0500

    moved connect function to client.js

--- play.js
+++ play.js
@@ -1,28 +1,3 @@
-
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("Connected");
-  });
-
-  conn.on("data", (message) => {
-    // code that does something when the connection is first established
-    console.log("Server sent: ", message);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
+const connect = require("./client");
 console.log("Connecting ...");
 connect();
